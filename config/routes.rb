b72beba3883eeea40a688c3f221c0ec88f381c17
Takuma <takumab@gmail.com>
2016-04-05 23:58:03 +0100

    Add navbar and image

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
   get 'static_pages/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
