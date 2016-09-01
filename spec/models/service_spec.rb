4904b901ac91261f2fb5b469b7443c54e93b7fe8
takumab <takumab@gmail.com>
2016-09-01 16:22:50 +0100

    Add user spec and user factory files

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -2,21 +2,19 @@ require 'rails_helper'
 
 describe Service, :type => :model do
 	
-
-	
-	describe "validity" do
+	describe "validations" do
 		subject { FactoryGirl.build(:service, name: "Example service", description: "Clean example", image_url: "example.jpg") }
-		it "should not validate service without a name" do
+		it "does not validate service without a name" do
 			subject.name = nil
 			expect(subject).not_to be_valid 
 		end
 
-		it "should not validate service without a description" do
+		it "does not validate service without a description" do
 			subject.description = nil
 			expect(subject).not_to be_valid 
 		end
 
-		it "should not validate service without a image" do
+		it "does not validate service without a image" do
 			subject.image_url = nil
 			expect(subject).not_to be_valid 
 		end
