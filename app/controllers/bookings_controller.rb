afeb47f3dcb475e41a2a518f8c3a9d1dcbd96d06
takumab <takumab@gmail.com>
2016-09-11 12:12:56 +0100

    Add months and glyphs to calendar

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,7 +3,7 @@ class BookingsController < ApplicationController
 	def index
 		@bookings = Booking.all
 		@bookings_by_date = @bookings.group_by(&:date) 
-		@date = params[:date] ? Date.parse(parse[:date]) : Date.today
+		@date = params[:date] ? Date.parse(params[:date]) : Date.today
 	end
 
 	def show
@@ -14,6 +14,7 @@ class BookingsController < ApplicationController
 	end
 
 	def create
+		
 	end
 
 	def destroy
